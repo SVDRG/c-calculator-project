0310bf0cdaaae3fa65ed9075458a540f0844bc38
SVDRG <dmsdyd741025@gmail.com>
2026-03-24 16:54:21 +0900

    Initial C calculator project with GitHub Actions

---
+++ src/div.c
@@ -0,0 +1,9 @@
+#include "calc.h"
+
+int divide(int a, int b, int *result) {
+    if (b == 0) {
+        return 0;
+    }
+    *result = a / b;
+    return 1;
+}
---
+++ src/print_result.c
@@ -0,0 +1,6 @@
+#include <stdio.h>
+#include "calc.h"
+
+void print_result(const char *label, int result) {
+    printf("%s °á°ú: %d\n", label, result);
+}
